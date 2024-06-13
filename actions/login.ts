fdc45edebc36f44eb6f845ec3e194ae612fd5488
holail14 <kev20duvin8@gmail.com>
2024-06-13 17:57:05 +0200

    Add Middleware & Login

--- actions/login.ts
+++ actions/login.ts
@@ -1,8 +1,11 @@
 "use server";
 
 import * as z from 'zod';
+import { AuthError } from 'next-auth';
 
+import { signIn } from '@/auth';
 import { LoginSchema } from '@/schemas';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values);
@@ -11,5 +14,26 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
         return { error: "Champs invalides !" }
     }
 
-    return { success: "Email envoyé !" }
+    const { email, password } = validatedFields.data;
+
+    try {
+        await signIn("credentials", {
+            email,
+            password,
+            redirectTo: DEFAULT_LOGIN_REDIRECT
+        })
+
+        return { success: "Connexion réussie !" }
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case "CredentialsSignin":
+                    return { error: "Identifiants incorrects !" }
+                default:
+                    return { error: "Une erreur est survenue !" }
+            }
+        }
+
+        throw error;
+    }
 }
