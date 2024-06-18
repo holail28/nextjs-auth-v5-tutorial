a714fc520ef094ee70b8874072518567718cebf6
holail14 <kev20duvin8@gmail.com>
2024-06-18 17:50:32 +0200

    Add Email verification

---
+++ app/auth/new-verification/page.tsx
@@ -0,0 +1,9 @@
+import { NewVerificationForm } from "@/components/auth/new-verification-form";
+
+const newVerificationPage = () => {
+    return (
+        <NewVerificationForm />
+    );
+}
+
+export default newVerificationPage;
