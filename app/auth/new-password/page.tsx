dff747a9c00c59560ee5b1bb36a6577f033a8f15
holail14 <kev20duvin8@gmail.com>
2024-06-19 17:32:21 +0200

    Add reset password form

---
+++ app/auth/new-password/page.tsx
@@ -0,0 +1,9 @@
+import { NewPasswordForm } from "@/components/auth/new-password-form";
+
+const newPasswordPage = () => {
+    return (
+        <NewPasswordForm />
+    );
+}
+ 
+export default newPasswordPage;
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,11 @@
 import * as z from 'zod';
 
+export const NewPasswordSchema = z.object({
+    password: z.string().min(6, {
+        message: "Minimum 6 caractères requis."
+    }),
+})
+
 export const ResetSchema = z.object({
     email: z.string().email({
         message: "L'email est requis."
